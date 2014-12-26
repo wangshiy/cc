59c3632a7452b5ba6ba2a238547df9aff7a92c94
wangshiy <wangshiy@umich.edu>
2014-12-26 17:47:14 -0500

    Two_sum

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -19,15 +19,14 @@ O(n) runtime, O(n) space – Hash table:
 We could reduce the runtime complexity of looking up a value to O(1) using a hash map that maps a value to its index.
 */
 
-
-
 #include <iostream>
 #include <utility>
+#include<map>
 
 using namespace std;
 
 pair<int,int> result;
-int target = 10;
+int target = 8;
 bool f = false;
 
 void Twosum_brute(int a[])
@@ -60,9 +59,42 @@ void Twosum_brute(int a[])
 	}
 }
 
+void Twosum_hash(int a[])
+{
+	map<int,int> m;
+	for (int i=0;i<5;i++)
+	{
+		m[target-a[i]]=i;
+	}
+	for (int i=0;i<5;i++)
+	{
+		if(m.find(a[i])==m.end())
+		{
+			continue;
+		}
+		else
+		{
+			result.first = i;
+			result.second = m[a[i]];
+			f=true;
+			break;
+		}
+	}
+	if(f==true)
+	{
+		cout<<"the first index:"<<result.first<<endl;
+		cout<<"the second index:"<<result.second<<endl;
+	}
+	else
+	{
+		cout<<"result not found"<<endl;
+	}
+}
+
 int main()
 {
 	int a[5]={1,2,4,7,9};
 	Twosum_brute(a);
+	Twosum_hash(a);
 	return 0;
 }
